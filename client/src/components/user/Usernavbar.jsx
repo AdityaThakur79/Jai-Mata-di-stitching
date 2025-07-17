2e3972c13983ba8579b089a66145e7b0a24389e0
Rahul <jagwanrahul500@gmail.com>
2025-07-17 23:20:27 +0530

    WIP: Pages

--- client/src/components/user/Usernavbar.jsx
+++ client/src/components/user/Usernavbar.jsx
@@ -138,13 +138,13 @@ const UserNavbar = () => {
               </span>
               <div className="absolute bottom-0 left-0 w-0 h-0.5 bg-yellow-500 transition-all duration-300 group-hover:w-full"></div>
             </a>
-            <a href="/about" className="relative group py-2">
+            <a href="/men" className="relative group py-2">
               <span className="text-gray-700 hover:text-yellow-600 transition-colors duration-200 text-sm font-medium tracking-wide uppercase">
                 Men
               </span>
               <div className="absolute bottom-0 left-0 w-0 h-0.5 bg-yellow-500 transition-all duration-300 group-hover:w-full"></div>
             </a>
-            <a href="/about" className="relative group py-2">
+            <a href="/women" className="relative group py-2">
               <span className="text-gray-700 hover:text-yellow-600 transition-colors duration-200 text-sm font-medium tracking-wide uppercase">
                 Women
               </span>
