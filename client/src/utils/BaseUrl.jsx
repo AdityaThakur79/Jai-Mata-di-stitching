14d0daebac622cb43e1e166d0d782694ffdb3eeb
Aditya Thakur <adityathakurbusiness@gmail.com>
2025-07-23 16:16:42 +0530

    Base url updated

--- client/src/utils/BaseUrl.jsx
+++ client/src/utils/BaseUrl.jsx
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:8080/api";
+// export const BASE_URL = "http://localhost:8080/api";
 // export const BASE_URL = "https://jai-mata-di-stitching.onrender.com/api";  
+export const BASE_URL = "https://jai-mata-di-stitching-1mic.onrender.com/api";  
